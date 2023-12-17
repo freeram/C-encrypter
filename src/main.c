c945c26e6e5edf445e296e823ecfb170a96669c9
freeram <kulikovpavel2801@gmail.com>
2023-12-17 13:56:15 -0700

    Add reading stdin, do not print linefeed

--- src/main.c
+++ src/main.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include "encryption.h"
-// #include "filereader.h"
+#include "filereader.h"
 
 int main(int argc, char *argv[])
 {
@@ -9,19 +9,19 @@ int main(int argc, char *argv[])
 
     blowfish_init(key, key_len);
 
-    uint8_t buffer[] = {0x48, 0x65, 0x6C, 0x6C, 0x6F, 0x20, 0x57, 0x6F, 0x72, 0x6C, 0x64, 0x21, 0x00, 0x00, 0x00, 0x00};
-    size_t buffer_size = sizeof(buffer) / sizeof(buffer[0]);
+    size_t buffer_size;
+    uint8_t *buffer = read_stdin_data(&buffer_size);
 
     printf("Original buffer:\n");
-    for (size_t i = 0; i < buffer_size; i++) {
-        printf("%02x ", buffer[i]);
+    for (size_t i = 0; i < buffer_size-1; i++) {
+        printf("%c", buffer[i]);
     }
     printf("\n");
 
     encrypt(buffer, buffer_size);
 
     printf("Encrypted buffer:\n");
-    for (size_t i = 0; i < buffer_size; i++) {
+    for (size_t i = 0; i < buffer_size-1; i++) {
         printf("%02x ", buffer[i]);
     }
     printf("\n");
@@ -29,8 +29,8 @@ int main(int argc, char *argv[])
     decrypt(buffer, buffer_size);
 
     printf("Decrypted buffer:\n");
-    for (size_t i = 0; i < buffer_size; i++) {
-        printf("%02x ", buffer[i]);
+    for (size_t i = 0; i < buffer_size-1; i++) {
+        printf("%c", buffer[i]);
     }
     printf("\n");
 
