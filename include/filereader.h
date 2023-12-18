df040d197ba7c21b93ed059e8a66856e774a11f7
freeram <kulikovpavel2801@gmail.com>
2023-12-18 14:35:12 -0700

    Add write_file_data, refactor

--- include/filereader.h
+++ include/filereader.h
@@ -1,17 +1,23 @@
 #ifndef FILE_READER_H
 #define FILE_READER_H
+
 #include <stdio.h>
 #include <stdint.h>
 #include <stdlib.h>
 
+// Initial buffer capacity for read_stdin_data and read_file_data
+// Buffer gets resized to exact needed size by end of execution
 #define INITIAL_CAPACITY 1024
 
-// Reads data from a file and returns a pointer to the data buffer.
-// The size of the data is stored in 'data_size'.
-uint8_t* read_file_data(const char* filename, size_t* buffer_size);
-
 // Reads data from stdin and returns a pointer to the data buffer.
-// The size of the data is stored in 'data_size'.
-uint8_t* read_stdin_data(size_t* buffer_size);
+// The size of the data is stored in 'buffer_size'.
+uint8_t *read_stdin_data(size_t *buffer_size);
+
+// Reads data from a file and returns a pointer to the data buffer.
+// The size of the data is stored in 'buffer_size'.
+uint8_t *read_file_data(const char *filename, size_t *buffer_size);
+
+// Writes data to a file and returns 1 if successful
+int write_file_data(const char *filename, const char *data);
 
 #endif
