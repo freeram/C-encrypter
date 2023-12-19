5a250550c6df95ead97b03e940852deeeb5cad14
freeram <kulikovpavel2801@gmail.com>
2023-12-18 17:19:58 -0700

    Determine endianness

--- include/encryption.h
+++ include/encryption.h
@@ -4,7 +4,13 @@
 #include <stdlib.h>
 #include <stdint.h>
 
-// Define any constants here, e.g., key sizes, block sizes
+// Determining endianness
+#if defined(__BYTE_ORDER__) && __BYTE_ORDER__ == __ORDER_BIG_ENDIAN__
+#define IS_BIG_ENDIAN 1
+#else
+#define IS_BIG_ENDIAN 0
+#endif
+
 void blowfish_init(uint8_t *key, size_t key_len);
 
 void encrypt(uint8_t *buffer, size_t buffer_size);
